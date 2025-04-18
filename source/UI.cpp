7e18de34ab3f0d4eda4fa691120e04dab8769ac8
Sune27 <suneshinebn@gmail.com>
2025-04-18 14:14:36 +0700

    theem chi so cua UI hover

--- source/UI.cpp
+++ source/UI.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+//button
+Button::Button(int x, int y, int width, int height, Color background, Color backgroundHover)
+{
+    this->x = x;
+    this->y = y;
+    this->width = width;
+    this->height = height;
+    this->background = background;
+    this->background_hover = backgroundHover;
+}
+
 //UI manager
 UI_Manager::UI_Manager()
 {
-    UI_Maps
+    UI_Maps[UIObject::EXIT_BUTTON] = make_unique<Button>(X_exitButton, Y_exitButton, Width_exitButton, Height_exitButton, Background_exitButton, BackgroundHover_exitButton);
 }
